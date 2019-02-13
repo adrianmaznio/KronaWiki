d1c3cf2533e429cf03f408d270086c3b5ef1ea30
Adrian Maznio <amaznio1@binghamton.edu>
2019-02-13 13:02:50 -0500

    Finished Spirits

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 var cookieParser = require('cookie-parser');
 var urlencodedParser = bodyParser.urlencoded({
-    extended: false
+  extended: false
 });
 const path = require("path");
 var async = require('async');
@@ -39,33 +39,33 @@ const app = express();
 //App configuration
 app.use(cookieParser());
 app.use(session({
-    secret: 'secret123',
-    saveUninitialized: true,
-    resave: true
+  secret: 'secret123',
+  saveUninitialized: true,
+  resave: true
 }));
 
 //express messages middleware
 app.use(require('connect-flash')());
 app.use(function(req, res, next) {
-    res.locals.messages = require('express-messages')(req, res);
-    next();
+  res.locals.messages = require('express-messages')(req, res);
+  next();
 });
 // express validator
 app.use(expressValidator({
-    errorFormatter: function(param, msg, value) {
-        var namespace = param.split('.'),
-            root = namspace.shift(),
-            formParam = root;
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.'),
+      root = namspace.shift(),
+      formParam = root;
 
-        while (namespace.length) {
-            formParam += '[' + namspace.shift() + ']';
-        }
-        return {
-            param: formParam,
-            msg: msg,
-            value: value
-        };
+    while (namespace.length) {
+      formParam += '[' + namspace.shift() + ']';
     }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
 }));
 
 app.use(express.static(__dirname + '/views'));
@@ -77,7 +77,7 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 
 //Routes
@@ -85,18 +85,18 @@ app.use(bodyParser.urlencoded({
 
 //Routes
 app.get('/', function(req, res) {
-    res.render('home');
+  res.render('home');
 });
 app.get('/home', function(req, res) {
-    res.render('home');
+  res.render('home');
 });
 app.get('/character/items/weapons', function(req, res) {
-    db.collection('weapons').find().toArray((err, result) => {
-        if (err) return console.log(err)
-        res.render('weapons', {
-            weapons: result
-        })
+  db.collection('weapons').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('weapons', {
+      weapons: result
     })
+  })
 });
 app.get('/character/items/weapons/:weaponID/details', urlencodedParser, weapon_controller.weapon_details);
 app.post('/dashboard/items/weapons/create', urlencodedParser, weapon_controller.weapon_create);
@@ -104,12 +104,12 @@ app.post('/dashboard/items/weapons/update', urlencodedParser, weapon_controller.
 app.post('/dashboard/items/weapons/delete', urlencodedParser, weapon_controller.weapon_delete);
 
 app.get('/character/items/armor', function(req, res) {
-    db.collection('armors').find().toArray((err, result) => {
-        if (err) return console.log(err)
-        res.render('armor', {
-            armor: result
-        })
+  db.collection('armors').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('armor', {
+      armor: result
     })
+  })
 });
 app.get('/character/items/armor/:armorID/details', urlencodedParser, armor_controller.armor_details);
 app.post('/dashboard/items/armor/create', urlencodedParser, armor_controller.armor_create);
@@ -117,32 +117,32 @@ app.post('/dashboard/items/armor/update', urlencodedParser, armor_controller.arm
 app.post('/dashboard/items/armor/delete', urlencodedParser, armor_controller.armor_delete);
 
 app.get('/dashboard/items', function(req, res) {
-    var locals = {};
-    var tasks = [
-        // Load Weapons
-        function(callback) {
-            db.collection('weapons').find({}).toArray(function(err, weapons) {
-                if (err) return callback(err);
-                locals.weapons = weapons;
-                callback();
-            });
-        },
-        // // Load Armor
-        function(callback) {
-            db.collection('armors').find({}).toArray(function(err, armor) {
-                if (err) return callback(err);
-                locals.armor = armor;
-                callback();
-            });
-        }
-    ];
+  var locals = {};
+  var tasks = [
+    // Load Weapons
+    function(callback) {
+      db.collection('weapons').find({}).toArray(function(err, weapons) {
+        if (err) return callback(err);
+        locals.weapons = weapons;
+        callback();
+      });
+    },
+    // // Load Armor
+    function(callback) {
+      db.collection('armors').find({}).toArray(function(err, armor) {
+        if (err) return callback(err);
+        locals.armor = armor;
+        callback();
+      });
+    }
+  ];
 
-    async.parallel(tasks, function(err) { //This function gets called after the two tasks have called their "task callbacks"
-        if (err) return next(err); //If an error occurred, let express handle it by calling the `next` function
-        // Here `locals` will be an object with `products` and `newsposts` keys
-        // Example: `locals = {products: [...], newsposts: [...]}`
-        res.render('manageitems', locals);
-    });
+  async.parallel(tasks, function(err) { //This function gets called after the two tasks have called their "task callbacks"
+    if (err) return next(err); //If an error occurred, let express handle it by calling the `next` function
+    // Here `locals` will be an object with `products` and `newsposts` keys
+    // Example: `locals = {products: [...], newsposts: [...]}`
+    res.render('manageitems', locals);
+  });
 });
 
 // app.get('/dashboardnewsposts', function(req, res) {
@@ -161,30 +161,30 @@ app.get('/dashboard/items', function(req, res) {
 
 //Guides
 app.get('/guide/', function(req, res) {
-    res.render('guide');
+  res.render('guide');
 });
 app.get('/guide/leveling', function(req, res) {
-    res.render('levelingguide');
+  res.render('levelingguide');
 });
 
 app.get('/guide/classes', function(req, res) {
-    res.render('classesguide');
+  res.render('classesguide');
 });
 
 app.get('/guide/everpower', function(req, res) {
-    res.render('everpowerguide');
+  res.render('everpowerguide');
 });
 
 app.get('/guide/crafting', function(req, res) {
-    res.render('craftingguide');
+  res.render('craftingguide');
 });
 
 app.get('/guide/spirits', function(req, res) {
-    res.render('spiritsguide');
+  res.render('spiritsguide');
 });
 
 app.get('/guide/highly-detailed-fully-graphical-guide', function(req, res) {
-    res.render('highly-detailed-fully-graphical-guide');
+  res.render('highly-detailed-fully-graphical-guide');
 });
 
 // app.get('/guides/:guideID/:guideName', urlencodedParser, guide_controller.guide_details);
@@ -200,5 +200,5 @@ app.post('/dashboard/items/consumables/delete', urlencodedParser, consumable_con
 
 //
 app.listen(process.env.PORT || 4000, () => {
-    console.log('Wiki is up and running!');
+  console.log('Wiki is up and running!');
 });
